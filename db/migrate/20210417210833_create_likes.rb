97e42d7f602e182cba76359f4e8deac8781f457c
wfswee <wfswee@chicagobooth.edu>
2021-04-17 21:11:08 +0000

    updated like migration file

--- db/migrate/20210417210833_create_likes.rb
+++ db/migrate/20210417210833_create_likes.rb
@@ -1,7 +1,7 @@
 class CreateLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :likes do |t|
-      t.references :fan, null: false, foreign_key: true
+      t.references :fan, null: false, foreign_key: { table_to: :users }
       t.references :photo, null: false, foreign_key: true
 
       t.timestamps
