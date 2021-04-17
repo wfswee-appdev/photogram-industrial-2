1065d48d10af55aae85f5d9fa56e3720122b5283
wfswee <wfswee@chicagobooth.edu>
2021-04-17 21:12:58 +0000

    fixed misspelling in likes migration file

--- db/migrate/20210417210833_create_likes.rb
+++ db/migrate/20210417210833_create_likes.rb
@@ -1,7 +1,7 @@
 class CreateLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :likes do |t|
-      t.references :fan, null: false, foreign_key: { table_to: :users }
+      t.references :fan, null: false, foreign_key: { to_table: :users }
       t.references :photo, null: false, foreign_key: true
 
       t.timestamps
