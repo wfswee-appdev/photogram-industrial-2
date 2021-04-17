c9d15867c7f56ec949e04cb48053e986e49f7729
wfswee <wfswee@chicagobooth.edu>
2021-04-17 20:58:18 +0000

    fixed error in follow_request migration file

--- db/migrate/20210417204736_create_follow_requests.rb
+++ db/migrate/20210417204736_create_follow_requests.rb
@@ -2,7 +2,7 @@ class CreateFollowRequests < ActiveRecord::Migration[6.1]
   def change
     create_table :follow_requests do |t|
       t.references :recipient, null: false, foreign_key: { to_table: :users }
-      t.references :sender, null: false, foreign_key:  foreign_key: { to_table: :users }
+      t.references :sender, null: false, foreign_key: { to_table: :users }
       t.string :status
 
       t.timestamps
