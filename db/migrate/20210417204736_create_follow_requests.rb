09af9fa44e32902fa92ce234427326b45461981c
wfswee <wfswee@chicagobooth.edu>
2021-04-17 20:52:27 +0000

    changed migration file

--- db/migrate/20210417204736_create_follow_requests.rb
+++ db/migrate/20210417204736_create_follow_requests.rb
@@ -1,8 +1,8 @@
 class CreateFollowRequests < ActiveRecord::Migration[6.1]
   def change
     create_table :follow_requests do |t|
-      t.references :recipient, null: false, foreign_key: true
-      t.references :sender, null: false, foreign_key: true
+      t.references :recipient, null: false, foreign_key: { to_table: :users }
+      t.references :sender, null: false, foreign_key:  foreign_key: { to_table: :users }
       t.string :status
 
       t.timestamps
