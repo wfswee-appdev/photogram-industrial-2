1e0ccde3b11f0c125f2b601895418d57064dd3fc
wfswee <wfswee@chicagobooth.edu>
2021-04-18 04:38:07 +0000

    direct

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -21,7 +21,7 @@
 #
 class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User", counter_cache: true
-  has_many :fans, through: :likes
+
   has_many :comments
   has_many :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :photos
-  has_many :comments
-  has_many :likes
+
+  has_many :comments, foreign_key: :author_id
+  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
+  has_many :likes, foreign_key: :fan_id
+  has_many :own_photos, foreign_key: :owner_id, class: "Photo"
 end
