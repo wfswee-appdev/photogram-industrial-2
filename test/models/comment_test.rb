3f071f8bd857aa6304bdb0a8dc9dab9798cb174b
wfswee <wfswee@chicagobooth.edu>
2021-04-18 04:21:38 +0000

    migrated

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,11 +3,21 @@
 # Table name: comments
 #
 #  id         :bigint           not null, primary key
-#  author_id  :bigint           not null
-#  photo_id   :bigint           not null
 #  body       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  author_id  :bigint           not null
+#  photo_id   :bigint           not null
+#
+# Indexes
+#
+#  index_comments_on_author_id  (author_id)
+#  index_comments_on_photo_id   (photo_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (author_id => users.id)
+#  fk_rails_...  (photo_id => photos.id)
 #
 require "test_helper"
 
