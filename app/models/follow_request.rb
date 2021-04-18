0e77231f9c0203b5ab68464880a5b9472b831bc6
wfswee <wfswee@chicagobooth.edu>
2021-04-18 20:01:01 +0000

    set enum scopes

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -22,5 +22,10 @@
 class FollowRequest < ApplicationRecord
   belongs_to :recipient, class_name: "User"
   belongs_to :sender, class_name: "User"
+
+  enum status: { pending: "pending", rejected: "rejected", accepted: "accepted" }
+  # defines positive and negative scopes
+  # scope :accepted, -> { where(status: "accepted") }
+  # scope :not_accepted, -> { where.not(status: "accepted") }
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -30,9 +30,9 @@ class User < ApplicationRecord
 
   has_many :comments, foreign_key: :author_id
   has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
-  has_many :accepted_sent_follow_requests, -> { where(status: "accepted") }, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
-  has_many :accepted_received_follow_requests, -> { where(status: "accepted") }, foreign_key: :recipient_id, class_name: "FollowRequest"
+  has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest"
   has_many :likes, foreign_key: :fan_id
   has_many :own_photos, foreign_key: :owner_id, class_name: "Photo"
   has_many :liked_photos, through: :likes, source: :photo
