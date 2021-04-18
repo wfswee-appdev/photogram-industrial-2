538946a2de3292f6b403fef6e77eef364f8c7f28
wfswee <wfswee@chicagobooth.edu>
2021-04-18 19:43:25 +0000

    added scopes to photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -29,4 +29,6 @@ class Photo < ApplicationRecord
   validates :caption, presence: true
   validates :image, presence: true
 
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+  scope :by_likes, -> { order(likes_count: :desc) }
 end
