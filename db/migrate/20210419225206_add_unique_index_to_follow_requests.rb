cb379c4c00cdd48334dc8d9f994218add23dd606
wfswee <wfswee@chicagobooth.edu>
2021-04-19 22:57:53 +0000

    updated unique index migration file

--- db/migrate/20210419225206_add_unique_index_to_follow_requests.rb
+++ db/migrate/20210419225206_add_unique_index_to_follow_requests.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexToFollowRequests < ActiveRecord::Migration[6.1]
   def change
-    add_index :follow_requests, [:recipients, :senders], unique: true
+    add_index :follow_requests, [:recipient_id, :sender_id], unique: true
   end
 end
