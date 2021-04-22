a1def694bbf866af02b72aba1f304c3d6c725998
wfswee <wfswee@chicagobooth.edu>
2021-04-22 04:43:08 +0000

    setup users#show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   root "photos#index"
 
+#  get "/users/:id" => "users#show", as: :user
+
+
   devise_for :users
 
   resources :comments
   resources :follow_requests
   resources :likes
   resources :photos
-
+  resources :users, only: :show
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
